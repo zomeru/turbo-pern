afd6c42352935b03b39b57b74269880ad11a7a4f
zomeru <zomerzxc.19@gmail.com>
2022-12-25 03:29:37 +0800

    Render config

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import App from './App';
+
+const URL = import.meta.env.VITE_GRAPHQL_URL;
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const URL_TWO = `https://${process.env.GRAPHQL_HOST}/graphql` as string;
+const URL_FINAL = URL || URL_TWO || "'http://localhost:8000/graphql'";
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql",
+  uri: URL_FINAL,
   cache: new InMemoryCache(),
 });
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
