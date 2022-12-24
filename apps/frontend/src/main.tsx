03c8624a5394df6f7fc7b163b916b92431b2cc0c
zomeru <zomerzxc.19@gmail.com>
2022-12-25 04:54:16 +0800

    Add some tweaks

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -5,7 +5,9 @@ import App from './App';
 
 const URL = import.meta.env.VITE_GRAPHQL_URL;
 // eslint-disable-next-line turbo/no-undeclared-env-vars
-const URL_TWO = `https://${import.meta.env.VITE_GRAPHQL_HOST}/graphql`;
+const URL_TWO = `https://${
+  import.meta.env.VITE_GRAPHQL_HOST
+}.onrender.com/graphql`;
 const URL_FINAL = URL || URL_TWO || "'http://localhost:8000/graphql'";
 
 const client = new ApolloClient({
